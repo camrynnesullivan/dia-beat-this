47e9de5b7e735eb412336925ba675020db233286
Nathan Pecchia <septumprince@MacBook-Pro.local>
2020-09-09 14:54:58 -0400

    Added Grid for cards

--- src/pages/ProgressPage.js
+++ src/pages/ProgressPage.js
@@ -1,21 +1,43 @@
 import React from "react"
-import Container from "@material-ui/core/Container"
 import BloodSugarCard from "../components/BloodSugarCard"
 import ChartCard from "../components/BloodSugarCard"
 import FoodSuggestedCard from "../components/BloodSugarCard"
 import SymptomsCard from "../components/BloodSugarCard"
+import Grid from '@material-ui/core/Grid';
+import { makeStyles } from '@material-ui/core/styles';
 
-
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  }
+}));
 
 function ProgressPage(props) {
+  const classes = useStyles();
 
-
-  return <Container maxWidth="sm" fixed>
-  <BloodSugarCard />
-  <ChartCard />
-  <FoodSuggestedCard />
-  <SymptomsCard />
-  </Container>
+  return (
+        <div className={classes.root}>
+<Grid
+  container
+  direction="column"
+  justify="center"
+  alignItems="center"
+  spacing={2}
+>
+    <Grid item>
+        <BloodSugarCard />
+    </Grid>
+    <Grid item>
+        <ChartCard />
+    </Grid>
+    <Grid item>
+        <FoodSuggestedCard />
+    </Grid>
+    <Grid item>
+        <SymptomsCard />
+    </Grid>
+  </Grid>
+  </div>)
 }
 
 export default ProgressPage
