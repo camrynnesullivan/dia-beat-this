e9e26b80a9446b0d52db8949ae08e3e335980ddc
Nathan Pecchia <septumprince@MacBook-Pro.local>
2020-09-09 13:57:50 -0400

    Connected Card to ProgressPage

--- src/pages/ProgressPage.js
+++ src/pages/ProgressPage.js
@@ -1,10 +1,13 @@
 import React from "react"
-
+import Container from "@material-ui/core/Container"
+import Card from "../components/Card"
 
 function ProgressPage(props) {
 
 
-  return <h1>ProgressPage</h1>
+  return <Container maxWidth="sm" fixed>
+  <Card />
+  </Container>
 }
 
 export default ProgressPage
