5e0573cdac9ec2269cc81925a94b58d081e86df2
Nathan Pecchia <septumprince@MacBook-Pro.local>
2020-09-09 14:24:03 -0400

    Added Styles to BloodSugarCard

--- src/components/BloodSugarCard.js
+++ src/components/BloodSugarCard.js
@@ -2,11 +2,10 @@ import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
 import CardActionArea from '@material-ui/core/CardActionArea';
-import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
-import CardMedia from '@material-ui/core/CardMedia';
-import Button from '@material-ui/core/Button';
+
 import Typography from '@material-ui/core/Typography';
+
 const useStyles = makeStyles({
   root: {
     maxWidth: 345,
@@ -15,7 +14,7 @@ const useStyles = makeStyles({
     height: 140,
   },
 });
-export default function MediaCard() {
+export default function BloodSugarCard() {
   const classes = useStyles();
   return (
     <Card className={classes.root}>
@@ -26,6 +25,7 @@ export default function MediaCard() {
           </Typography>
           <Typography gutterBottom variant="h5" component="h2">
             {140} mg/dL
+          <i class="fas fa-tint"></i>
           </Typography>
           <Typography variant="body2" color="textSecondary" component="p">
             Looks good!
