4f3398008de51cf6c45da41efe630aeca7cbcce3
Camrynne Sullivan <camrynne.sullivan@gmail.com>
2020-09-09 15:00:42 -0400

    Updated App.js and index.js in src

--- client/src/App.js
+++ client/src/App.js
@@ -1,1 +1,17 @@
-// import React from 'react';
+import React from "react";
+import { useGlobalContext } from "./context/GlobalContext";
+import AuthenticatedApp from "./components/AuthenticatedApp";
+import UnauthenticatedApp from "./components/UnauthenticatedApp";
+import "./App.css";
+
+function App() {
+  const [state, dispatch] = useGlobalContext();
+
+  return (
+    <div className="App">
+      {state.user.token ? <AuthenticatedApp /> : <UnauthenticatedApp />}
+    </div>
+  );
+}
+
+export default App;
