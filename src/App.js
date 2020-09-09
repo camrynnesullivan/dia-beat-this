82c24bacdf6a969006df0e4a8983ea886420128f
Nathan Pecchia <septumprince@MacBook-Pro.local>
2020-09-09 15:05:01 -0400

    Added NavBar from MaterialUI

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React from "react"
 import ProgressPage from "./pages/ProgressPage"
+import AppBar from "./components/AppBar"
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
 class App extends React.Component {
 
   render() {
     return (
+      <Router>
+      <AppBar />
       <ProgressPage />
+      </Router>
     )
   }
 }
