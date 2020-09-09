5beea0a80e37ee9556c5fec22ae760f5f3b94590
Nathan Pecchia <septumprince@MacBook-Pro.local>
2020-09-09 16:08:58 -0400

    Added select option to form

--- src/pages/InputPage.js
+++ src/pages/InputPage.js
@@ -1,10 +1,23 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import TextField from '@material-ui/core/TextField';
+import MenuItem from '@material-ui/core/MenuItem';
+import Card from "@material-ui/core/MenuItem"
+
+const currencies = [
+  {
+    value: 'false',
+    label: "I'm about to eat",
+  },
+  {
+    value: 'true',
+    label: 'I already ate',
+  },
+];
 
 const useStyles = makeStyles((theme) => ({
   root: {
-    '& > *': {
+    '& .MuiTextField-root': {
       margin: theme.spacing(1),
       width: '25ch',
     },
@@ -13,12 +26,33 @@ const useStyles = makeStyles((theme) => ({
 
 function InputPage(props) {
   const classes = useStyles();
+  const [currency, setCurrency] = React.useState('EUR');
+
+  const handleChange = (event) => {
+    setCurrency(event.target.value);
+  };
 
   return (
     <form className={classes.root} noValidate autoComplete="off">
-      <TextField id="standard-basic" label="Standard" />
-      <TextField id="filled-basic" label="Filled" variant="filled" />
-      <TextField id="outlined-basic" label="Outlined" variant="outlined" />
+      <div><TextField id="standard-basic" label="Standard" /></div>
+      <div><TextField id="filled-basic" label="Filled" variant="filled" /></div>
+      <div><TextField id="outlined-basic" label="Outlined" variant="outlined" /></div>
+      <div>
+        <TextField
+          id="standard-select-currency"
+          select
+          label="Select"
+          value={currency}
+          onChange={handleChange}
+          helperText="When are you measuring?"
+        >
+          {currencies.map((option) => (
+            <MenuItem key={option.value} value={option.value}>
+              {option.label}
+            </MenuItem>
+          ))}
+        </TextField>
+      </div>
     </form>
   );
 }
