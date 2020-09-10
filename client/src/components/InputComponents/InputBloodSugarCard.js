cf0ad230cbbee3269f995530be03ae2b21e24635
Nathan Pecchia <septumprince@MacBook-Pro.local>
2020-09-10 17:04:47 -0400

    Created Cards for inputs

---
+++ client/src/components/InputComponents/InputBloodSugarCard.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
+import Grid from '@material-ui/core/Grid';
+import Card from '@material-ui/core/Card';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+  },
+}));
+
+export default function InputBloodSugarCard(props) {
+  const classes = useStyles();
+
+  return (
+    <Card>
+      InputBloodSugarCard
+    </Card>
+
+)
+}
--- client/src/pages/InputPage.js
+++ client/src/pages/InputPage.js
@@ -1,78 +1,19 @@
-import React from 'react';
+import React, { useState } from "react";
 import { makeStyles } from '@material-ui/core/styles';
-import TextField from '@material-ui/core/TextField';
-import MenuItem from '@material-ui/core/MenuItem';
-import Card from "@material-ui/core/MenuItem"
-import Radio from '@material-ui/core/Radio';
-import RadioGroup from '@material-ui/core/RadioGroup';
-import FormControlLabel from '@material-ui/core/FormControlLabel';
-import FormControl from '@material-ui/core/FormControl';
-import FormLabel from '@material-ui/core/FormLabel';
-
-
-const currencies = [
-  {
-    value: 'false',
-    label: "I'm about to eat",
-  },
-  {
-    value: 'true',
-    label: 'I already ate',
-  },
-];
-
-const useStyles = makeStyles((theme) => ({
-  root: {
-    '& .MuiTextField-root': {
-      margin: theme.spacing(1),
-      width: '25ch',
-    },
-  },
-}));
+import InputFoodCard from "../components/InputComponents/InputFoodCard"
+import InputBloodSugarCard from "../components/InputComponents/InputBloodSugarCard"
 
 function InputPage(props) {
-  const classes = useStyles();
-  const [currency, setCurrency] = React.useState('EUR');
-  const [value, setValue] = React.useState('female');
-
-  // const handleChange = (event) => {
-  //   setCurrency(event.target.value);
-  // };
-
-  const handleChange = (event) => {
-    setValue(event.target.value);
-  };
+  const [inputChoice, setInputChoice] = useState("Food");
 
   return (
-    <form className={classes.root} noValidate autoComplete="off">
-      <div><TextField id="standard-basic" label="Standard" /></div>
-      <div><TextField id="filled-basic" label="Filled" variant="filled" /></div>
-      <div><TextField id="outlined-basic" label="Outlined" variant="outlined" /></div>
       <div>
-        <TextField
-          id="standard-select-currency"
-          select
-          label="Select"
-          value={currency}
-          onChange={handleChange}
-          helperText="When are you measuring?"
-        >
-          {currencies.map((option) => (
-            <MenuItem key={option.value} value={option.value}>
-              {option.label}
-            </MenuItem>
-          ))}
-        </TextField>
+        { inputChoice === "Food" ? 
+          (<InputFoodCard />) 
+          :
+          (<InputBloodSugarCard />)}
       </div>
-      <FormControl component="fieldset">
-  <FormLabel component="legend">What would you like to track?</FormLabel>
-  <RadioGroup aria-label="gender" name="gender1" value={value} onChange={handleChange}>
-    <FormControlLabel value="food" control={<Radio />} label="Food" />
-    <FormControlLabel value="bloodSugar" control={<Radio />} label="Blood Sugar" />
-  </RadioGroup>
-</FormControl>
-    </form>
-  );
+      )
 }
 
 export default InputPage
