bdabcb6e133fc96e27c5663ae075440303dddd28
Nathan Pecchia <septumprince@macbook-pro.home>
2020-09-10 18:19:53 -0400

    Added buttons to input page

--- client/src/pages/InputPage.js
+++ client/src/pages/InputPage.js
@@ -1,17 +1,54 @@
 import React, { useState } from "react";
 import { makeStyles } from '@material-ui/core/styles';
+import Card from "@material-ui/core/Card"
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+
 import InputFoodCard from "../components/InputComponents/InputFoodCard"
 import InputBloodSugarCard from "../components/InputComponents/InputBloodSugarCard"
 
+const useStyles = makeStyles({
+  root: {
+    minWidth: 275,
+  },
+  bullet: {
+    display: 'inline-block',
+    margin: '0 2px',
+    transform: 'scale(0.8)',
+  },
+  title: {
+    fontSize: 14,
+  },
+  pos: {
+    marginBottom: 12,
+  },
+});
+
+
 function InputPage(props) {
-  const [inputChoice, setInputChoice] = useState("Food");
+  const classes = useStyles();
+  const [inputChoice, setInputChoice] = useState(null);
 
   return (
       <div>
-        { inputChoice === "Food" ? 
-          (<InputFoodCard />) 
-          :
-          (<InputBloodSugarCard />)}
+        <Card>
+        <CardContent>
+        <Typography className={classes.title} color="textSecondary" gutterBottom>
+          Let's figure out your insulin!
+        </Typography>
+        <Typography variant="h4" component="h4">
+       What would you like to track?
+        </Typography>
+      </CardContent>
+      <CardActions>
+        <Button size="small" color="secondary">Track Food</Button>
+        <Button size="small" color="primary">Track Blood Sugar</Button>
+      </CardActions>
+        </Card>
+        { inputChoice === "Food" && (<InputFoodCard />)}
+        { inputChoice === "BloodSugar" && (<InputBloodSugarCard />)}
       </div>
       )
 }
