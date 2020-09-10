9472672a1c355bda1e2c1cda8951271109d697d7
Nathan Pecchia <septumprince@MacBook-Pro.local>
2020-09-10 15:39:13 -0400

    Added switch to Authorized App

--- client/src/components/AuthenticatedApp.js
+++ client/src/components/AuthenticatedApp.js
@@ -1,12 +1,18 @@
 import React from "react";
 import { useGlobalContext } from "../context/GlobalContext";
 import { LOGOUT } from "../context/actions";
-import DenseAppBar from "./AppBar"
+import NavBar from "./NavBar"
+import HomePage from "../pages/HomePage"
+import InputPage from "../pages/InputPage"
+import ProgressPage from "../pages/ProgressPage"
+import { Route, Switch } from "react-router-dom";
+import { BrowserRouter as Router } from "react-router-dom";
 
 const AuthenticatedApp = () => {
   const [state, dispatch] = useGlobalContext();
 
   const handleLogout = () => {
+    console.log("Clicked!")
     dispatch({
       type: LOGOUT
     });
@@ -14,8 +20,16 @@ const AuthenticatedApp = () => {
 
   return (
     <div>
-      {/* <DenseAppBar /> */}
-      <button onClick={handleLogout}>Logout</button>
+      <Router>
+      <NavBar handleLogout={handleLogout}/>
+      <Switch>
+        <Route exact from="/" render={props => <HomePage {...props} />} />
+        <Route exact path="/input" render={props => <InputPage {...props} />} />
+        <Route exact path="/progress" render={props => <ProgressPage {...props} />} />
+      </Switch>
+      </Router>
+
+      {/* <button onClick={handleLogout}>Logout</button> */}
     </div>
   )
 }
