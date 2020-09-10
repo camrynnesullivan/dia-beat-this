39403a567751e2ede09b7ff09a78f1f211a546cc
Nathan Pecchia <septumprince@MacBook-Pro.local>
2020-09-10 16:24:33 -0400

    Added Hero to main page

---
+++ client/src/components/Hero.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Container from "@material-ui/core/Container";
+
+function Hero(props) {
+  return (
+      <Container>
+        <h1>Welcome to our App</h1>
+      </Container>
+  );
+}
+
+export default Hero;
