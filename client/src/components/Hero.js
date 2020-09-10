b8f525414913c280afd6685f4a39a3313a19f644
Nathan Pecchia <septumprince@MacBook-Pro.local>
2020-09-10 16:46:52 -0400

    Added to Hero

--- client/src/components/Hero.js
+++ client/src/components/Hero.js
@@ -1,12 +1,63 @@
-import React from "react";
-import Container from "@material-ui/core/Container";
+import React from 'react';
+import PropTypes from 'prop-types';
+import { makeStyles } from '@material-ui/core/styles';
+import Paper from '@material-ui/core/Paper';
+import Typography from '@material-ui/core/Typography';
+import Grid from '@material-ui/core/Grid';
+import Link from '@material-ui/core/Link';
 
-function Hero(props) {
+const useStyles = makeStyles((theme) => ({
+  hero: {
+    position: 'relative',
+    backgroundColor: theme.palette.grey[800],
+    color: theme.palette.common.white,
+    marginTop: 10,
+    marginBottom: 10,
+    backgroundImage: 'url(https://source.unsplash.com/random)',
+    backgroundSize: 'cover',
+    backgroundRepeat: 'no-repeat',
+    backgroundPosition: 'center',
+  },
+  overlay: {
+    position: 'absolute',
+    top: 0,
+    bottom: 0,
+    right: 0,
+    left: 0,
+    backgroundColor: 'rgba(0,0,0,.3)',
+  },
+  heroContent: {
+    position: 'relative',
+    padding: theme.spacing(3),
+    [theme.breakpoints.up('md')]: {
+      padding: theme.spacing(6),
+      paddingRight: 0,
+    },
+  },
+}));
+
+export default function Hero(props) {
+  const classes = useStyles();
+
   return (
-      <Container>
-        <h1>Welcome to our App</h1>
-      </Container>
+    <Paper className={classes.hero}>
+      <div className={classes.overlay} />
+      <Grid container>
+        <Grid item md={6}>
+          <div className={classes.heroContent}>
+            <Typography component="h1" variant="h3" color="inherit" gutterBottom>
+              Welcome to our Application
+            </Typography>
+            <Typography variant="h5" color="inherit" paragraph>
+              Subtitle
+            </Typography>
+          </div>
+        </Grid>
+      </Grid>
+    </Paper>
   );
 }
 
-export default Hero;
+Hero.propTypes = {
+  post: PropTypes.object,
+};
