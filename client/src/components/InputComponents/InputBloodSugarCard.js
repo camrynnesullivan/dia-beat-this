37b8f51ee8ab6dd88393999061e603a138246d1b
Nathan Pecchia <septumprince@macbook-pro.home>
2020-09-10 20:53:38 -0400

    Added function to render appropriate inputs based on radio selection

--- client/src/components/InputComponents/InputBloodSugarCard.js
+++ client/src/components/InputComponents/InputBloodSugarCard.js
@@ -4,18 +4,50 @@ import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
 import Grid from '@material-ui/core/Grid';
 import Card from '@material-ui/core/Card';
+import FormLabel from '@material-ui/core/FormLabel';
+import FormControl from '@material-ui/core/FormControl';
+import FormGroup from '@material-ui/core/FormGroup';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import FormHelperText from '@material-ui/core/FormHelperText';
+import Switch from '@material-ui/core/Switch';
+import Radio from '@material-ui/core/Radio';
+import RadioGroup from '@material-ui/core/RadioGroup';
+
 
 const useStyles = makeStyles((theme) => ({
   root: {
   },
 }));
 
-export default function InputBloodSugarCard(props) {
+export default function InputFoodCard(props) {
   const classes = useStyles();
+  const [radio, setRadio] = React.useState(false)
+  
+  const handleRadio = (e) => {
+    setRadio(!radio);
+  };
 
   return (
     <Card>
-      InputBloodSugarCard
+  <FormControl component="fieldset">
+      <FormLabel component="legend">When are you measuring?</FormLabel>
+      <RadioGroup row aria-label="position" name="position" defaultValue="top" onChange={handleRadio}>
+
+        <FormControlLabel
+          value="before"
+          control={<Radio color="primary" />}
+          label="Before Food"
+          labelPlacement="bottom"
+        />
+            <FormControlLabel
+          value="after"
+          control={<Radio color="primary" />}
+          label="After Food"
+          labelPlacement="bottom"
+        />
+      </RadioGroup>
+      {radio ?  <h1>Before food questions</h1> : "after" && <h1>After food questions</h1>}
+    </FormControl>
     </Card>
 
 )
--- client/src/components/InputComponents/InputFoodCard.js
+++ client/src/components/InputComponents/InputFoodCard.js
@@ -4,6 +4,13 @@ import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
 import Grid from '@material-ui/core/Grid';
 import Card from '@material-ui/core/Card';
+import FormLabel from '@material-ui/core/FormLabel';
+import FormControl from '@material-ui/core/FormControl';
+import FormGroup from '@material-ui/core/FormGroup';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import FormHelperText from '@material-ui/core/FormHelperText';
+import Switch from '@material-ui/core/Switch';
+
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -12,10 +19,24 @@ const useStyles = makeStyles((theme) => ({
 
 export default function InputFoodCard(props) {
   const classes = useStyles();
+  const [state, setState] = React.useState(false)
+  
+  const handleChange = (event) => {
+    setState(!state);
+  };
 
   return (
     <Card>
-      InputFoodCard
+      <FormControl component="fieldset">
+      <FormLabel component="legend">Are you tracking before or after you eat?</FormLabel>
+      <FormGroup>
+        <FormControlLabel
+          control={<Switch checked={state.gilad} onChange={handleChange} name="gilad" />}
+          label="Gilad Gray"
+        />
+      </FormGroup>
+      <FormHelperText>Be careful</FormHelperText>
+    </FormControl>
     </Card>
 
 )
