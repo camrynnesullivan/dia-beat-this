12e3bddf34d6334ed5b0cb1be9718a856be6ac99
Natallia Stelmakh <stnata.coding@gmail.com>
2020-09-11 11:02:27 -0400

    Added accordions to warnings cards

--- client/src/components/WarningsComponents/HighLevelCard.js
+++ client/src/components/WarningsComponents/HighLevelCard.js
@@ -4,7 +4,8 @@ import Card from "@material-ui/core/Card";
 import CardActionArea from "@material-ui/core/CardActionArea";
 import CardContent from "@material-ui/core/CardContent";
 import Typography from "@material-ui/core/Typography";
-import { HBSTodo } from "./HighSugarTodo";
+import HBSTodo from "./HighSugarTodo";
+
 const useStyles = makeStyles({
   root: {
     minWidth: 400,
