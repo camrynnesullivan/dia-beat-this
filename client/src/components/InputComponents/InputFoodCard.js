b6c7e0d32192749caf4c231861c69c8b919b5c17
Nathan Pecchia <septumprince@MacBook-Pro-2.local>
2020-09-11 09:28:04 -0400

    working

--- client/src/components/InputComponents/InputFoodCard.js
+++ client/src/components/InputComponents/InputFoodCard.js
@@ -2,7 +2,7 @@ import React, {useState} from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import TextField from '@material-ui/core/TextField';
 import axios from 'axios';
-import Grid from '@material-ui/core/Grid';
+import Button from '@material-ui/core/Button';
 import Card from '@material-ui/core/Card';
 import FormLabel from '@material-ui/core/FormLabel';
 import FormControl from '@material-ui/core/FormControl';
@@ -23,43 +23,49 @@ export default function InputFoodCard(props) {
   })
   
   const handleInputChange = event => {
+
     const { name, value } = event.target;
-    this.setState({
+    setState({
       [name]: value
     });
+    console.log(state.q)
   };
 
   const getFood = () => {
-    axios.get("https://nutritionix-api.p.rapidapi.com/v1_1/search/" + this.state.q)
-      .then(res =>
-        {console.log(res.data);
-        this.setState({
-          foods: res.data
-        })}
-      )
-      .catch(() =>
-        this.setState({
-          foods: []
-        })
-      );
+    console.log("Working")
+      fetch("https://nutritionix-api.p.rapidapi.com/v1_1/search/" + state.q + "?fields=item_name%252Citem_id%252Cbrand_name%252Cnf_calories%252Cnf_total_fat", {
+        "method": "GET",
+        "headers": {
+          "x-rapidapi-host": "nutritionix-api.p.rapidapi.com",
+          "x-rapidapi-key": "6c45312c83msh64109d7b4df795dp1feb3bjsn924eea0bc390"
+        }
+      })
+      .then(response => {
+        console.log(response);
+      })
+      .catch(err => {
+        console.log(err);
+      });
   };
 
   const handleFormSubmit = event => {
     event.preventDefault();
-    this.getFood();
+    getFood();
   };
 
   return (
 <InputFormGrid>
-      <FormGroup className={classes.formElements} noValidate autoComplete="off" onChange={handleInputChange} onSubmit={handleFormSubmit} value={state.q}>
+      <FormGroup className={classes.formElements} noValidate autoComplete="off" >
          <FormLabel>What did you eat?</FormLabel>
-          <TextField id="filled-basic" label="Food" variant="filled"/>
-          <TextField id="filled-basic" variant="filled" InputProps={{
+          <TextField onChange={handleInputChange} value={state.q} id="filled-basic" label="Food" variant="filled"/>
+          {/* <TextField id="filled-basic" variant="filled" InputProps={{
             endAdornment: <InputAdornment position="end">servings</InputAdornment>,
-          }}/>
+          }}/> */}
+          <Button type="submit" variant="contained" color="secondary" onClick={handleFormSubmit}>Submit</Button>
       </FormGroup>
       <FormGroup>
       </FormGroup>
+
 </InputFormGrid>
 
 )
