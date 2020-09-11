9e75ccac9126b4ae94317003926f5678886d139e
Nathan Pecchia <septumprince@MacBook-Pro-2.local>
2020-09-11 11:50:19 -0400

    Added changes to WARNING cards

--- client/src/components/WarningsComponents/HighSugarTodo.js
+++ client/src/components/WarningsComponents/HighSugarTodo.js
@@ -6,14 +6,19 @@ import AccordionDetails from "@material-ui/core/AccordionDetails";
 import AccordionSummary from "@material-ui/core/AccordionSummary";
 import Typography from "@material-ui/core/Typography";
 import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import { faTint } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 const useStyles = makeStyles({
   root: {
     width: "100%",
+    backgroundColor: 'rgba(0, 0, 0, .03)',
   },
-  media: {
-    height: 140,
-  },
+  dropDown: {
+    margin: 30
+  }
 });
 
 export default function HBSTodo() {
@@ -28,21 +33,22 @@ export default function HBSTodo() {
           aria-controls="additional-actions1-content"
           id="additional-actions1-header"
         >
-          <Typography>
-            <h2>{treatingHBS.title}</h2>
+          <Typography variant="p" className={classes.title}>
+          {treatingHBS.title}
           </Typography>
         </AccordionSummary>
-        <AccordionDetails>
-          <Typography color="textSecondary">
-            <h3>{treatingHBS.subtitle}</h3>
-            <ul>
-              <li>{treatingHBS.todos[1]}</li>
-              <li>{treatingHBS.todos[2]}</li>
-              <li>{treatingHBS.todos[3]}</li>
-            </ul>
-            <h3>{treatingHBS.warning}</h3>
-          </Typography>
-        </AccordionDetails>
+        <div className={classes.dropDown}>
+          <Typography color="textSecondary" variant="h5">
+            {treatingHBS.subtitle}</Typography>
+            <Typography display="inital">
+            <List>
+              <ListItem><FontAwesomeIcon icon={faTint} pull="left" color="red" size="lg" />{treatingHBS.todos[1]}</ListItem>
+              <ListItem><FontAwesomeIcon icon={faTint} pull="left" color="red" size="lg" />{treatingHBS.todos[2]}</ListItem>
+              <ListItem><FontAwesomeIcon icon={faTint} pull="left" color="red" size="lg" />{treatingHBS.todos[3]}</ListItem>
+            </List>
+            </Typography>
+            <Typography variant="h6" >{treatingHBS.warning}</Typography>
+        </div>
       </Accordion>
     </div>
   );
