6ec898a517f1bae42e952a2f92ea9a52849bc4a1
Nathan Pecchia <septumprince@macbook-pro.home>
2020-09-10 22:20:55 -0400

    Working

--- client/src/components/InputComponents/InputFoodCard.js
+++ client/src/components/InputComponents/InputFoodCard.js
@@ -1,7 +1,7 @@
 import React, {useState} from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import TextField from '@material-ui/core/TextField';
-import Button from '@material-ui/core/Button';
+import axios from 'axios';
 import Grid from '@material-ui/core/Grid';
 import Card from '@material-ui/core/Card';
 import FormLabel from '@material-ui/core/FormLabel';
@@ -9,7 +9,6 @@ import FormControl from '@material-ui/core/FormControl';
 import FormGroup from '@material-ui/core/FormGroup';
 import InputFormGrid from './InputFormGrid';
 import InputAdornment from '@material-ui/core/InputAdornment';
-
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -23,36 +22,36 @@ export default function InputFoodCard(props) {
     q: ""
   })
   
-  handleInputChange = event => {
+  const handleInputChange = event => {
     const { name, value } = event.target;
     this.setState({
       [name]: value
     });
   };
 
-  getFood = () => {
-    API.getFood(this.state.q)
+  const getFood = () => {
+    axios.get("https://nutritionix-api.p.rapidapi.com/v1_1/search/" + this.state.q)
       .then(res =>
+        {console.log(res.data);
         this.setState({
           foods: res.data
-        })
+        })}
       )
       .catch(() =>
         this.setState({
-          foods: [],
-          message: "No matching food in database"
+          foods: []
         })
       );
   };
 
-  handleFormSubmit = event => {
+  const handleFormSubmit = event => {
     event.preventDefault();
     this.getFood();
   };
 
   return (
 <InputFormGrid>
-      <FormGroup className={classes.formElements} noValidate autoComplete="off" onChange={handleInputChange} onSubmit={handleSubmit}>
+      <FormGroup className={classes.formElements} noValidate autoComplete="off" onChange={handleInputChange} onSubmit={handleFormSubmit} value={state.q}>
          <FormLabel>What did you eat?</FormLabel>
           <TextField id="filled-basic" label="Food" variant="filled"/>
           <TextField id="filled-basic" variant="filled" InputProps={{
