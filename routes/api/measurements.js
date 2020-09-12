2d1c1b20cc9fb337332d814dc49c47474f242369
Hachem Said <labsar.ahmed@outlook.fr>
2020-09-12 18:40:57 -0400

    working on db

--- routes/api/measurements.js
+++ routes/api/measurements.js
@@ -2,15 +2,17 @@ const router = require("express").Router();
 const measurementController = require("../../controllers/measurementController");
 
 // Matches with "/api/books"
-router.route("/")
+router
+  .route("/")
   .get(measurementController.findAll)
   .post(measurementController.create);
 
 // Matches with "/api/books/:id"
-router
-  .route("/:id")
-  .get(measurementController.findById)
-  .put(measurementController.update)
-  .delete(measurementController.remove);
-
+// router
+//   .route("/:id")
+//   .get(measurementController.findById)
+//   .put(measurementController.update)
+//   .delete(measurementController.remove);
+Router.route("/api/measurements").post(measurementController.createNew);
+// .get(userController.getAllUsers)
 module.exports = router;
