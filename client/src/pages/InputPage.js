961a664188d3a267cb7404478c52b0e81d0bb35d
Nathan Pecchia <septumprince@macbook-pro-2.home>
2020-09-12 10:49:36 -0400

    Working on tabs

--- client/src/pages/InputPage.js
+++ client/src/pages/InputPage.js
@@ -5,11 +5,36 @@ import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
 import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
-
+import Paper from '@material-ui/core/Paper';
+import Tab from '@material-ui/core/Tab';
+import Box from '@material-ui/core/Box';
+import Tabs from '@material-ui/core/Tabs';
 import InputFoodCard from "../components/InputComponents/InputFoodCard"
 import InputBloodSugarCard from "../components/InputComponents/InputBloodSugarCard"
 import InputA1CCard from "../components/InputComponents/InputA1CCard"
 import CardGrid from "../components/CardGrid"
+
+function TabPanel(props) {
+  const { children, value, index, ...other } = props;
+
+  return (
+    <div
+      role="tabpanel"
+      hidden={value !== index}
+      id={`simple-tabpanel-${index}`}
+      aria-labelledby={`simple-tab-${index}`}
+      {...other}
+    >
+      {value === index && (
+        <Box p={3}>
+          <Typography>{children}</Typography>
+        </Box>
+      )}
+    </div>
+  );
+}
+
+
 
 const useStyles = makeStyles({
   root: {
@@ -33,11 +58,17 @@ function InputPage(props) {
   const classes = useStyles();
   const [inputChoice, setInputChoice] = useState(null);
   const [showTitle, setShowTitle] = useState(true);
+  const [value, setValue] = React.useState(0);
 
 const handleTrackButton = (buttonChoice) => {
     setInputChoice(buttonChoice)
     setShowTitle(false)
   }
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
+
 
   const buttonOptions = [
     {
@@ -62,7 +93,31 @@ const handleTrackButton = (buttonChoice) => {
 
   return (
       <CardGrid>
-        <Card>
+<Paper className={classes.root}>
+      <Tabs
+        value={value}
+        onChange={handleChange}
+        indicatorColor="primary"
+        textColor="primary"
+        centered
+      >
+        <Tab label="Item One" />
+        <Tab label="Item Two" />
+        <Tab label="Item Three" />
+      </Tabs>
+      <TabPanel value={value} index={0}>
+        Item One
+      </TabPanel>
+      <TabPanel value={value} index={1}>
+        Item Two
+      </TabPanel>
+      <TabPanel value={value} index={2}>
+        Item Three
+      </TabPanel>
+    </Paper>
+
+{/* 
+        {/* <Card>
         {showTitle &&<CardContent>
         <Typography className={classes.title} color="textSecondary" gutterBottom>
           Let's figure out your insulin!
@@ -84,7 +139,7 @@ const handleTrackButton = (buttonChoice) => {
         </Card>
         { inputChoice === "Food" && (<InputFoodCard />)}
         { inputChoice === "BloodSugar" && (<InputBloodSugarCard />)}
-        { inputChoice === "A1C" && (<InputA1CCard />)}
+        { inputChoice === "A1C" && (<InputA1CCard />)} */} */}
       </CardGrid>
       )
 }
