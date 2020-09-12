17a8861e66a0df6c8ca385b56ade1eda96c50a0a
Nathan Pecchia <septumprince@macbook-pro-2.home>
2020-09-12 08:15:31 -0400

    Fixed use effect

--- client/src/pages/ProgressPage.js
+++ client/src/pages/ProgressPage.js
@@ -25,37 +25,43 @@ function ProgressPage(props) {
     warning: "normal"
   }
   // Last Measurement from database
-  const [bloodSugar, setBloodSugar] = useState()
-  const [afterMeal, setAfterMeal] = useState()
+  const [bloodSugar, setBloodSugar] = useState(180)
+  const [afterMeal, setAfterMeal] = useState(true)
 
   // Hooks rendering the appropiate cards based on blood sugar range
   const [level, setLevel] = useState(normal)
 
-
-  // The beginning of this function needs to call the database and get information on the last recorded measurement.  There are example values here for now. The code written here might be better in a callback function, after useEffect makes the call to the databse.
+  // These values must be set afer the database is reached.
+  // setBloodSugar(data.enteredGlucose)
+  // setAfterMeal(data.afterMeal)
+  
 useEffect(() => {
-  // Play with these values to see how they render appropriately!
-  setBloodSugar(200)
-  setAfterMeal(true)
-  // After those values are set, this function sets the level. It should be done in a callback.; It will work if you try to uncomment, biut your app will immediately crash as it creates an infinite loop here.
-    // if (!afterMeal) {
-    //      if (bloodSugar < 80){
-    //       setLevel(low)
-    //     } else if (bloodSugar > 130) {
-    //       setLevel(high)
-    //     } else {
-    //       setLevel(normal)}
-    //     } else {
-    //       if (bloodSugar < 130){
-    //       setLevel(low)
-    //     } else if (bloodSugar > 180){
-    //       setLevel(high)
-    //     } else {setLevel(normal)}
-    //   }
-})
+    if (!afterMeal) {
+         if (bloodSugar < 80){
+          setLevel(low)
+        } else if (bloodSugar > 130) {
+          setLevel(high)
+        } else {
+          setLevel(normal)}
+        } else {
+          if (bloodSugar < 130){
+          setLevel(low)
+        } else if (bloodSugar > 180){
+          setLevel(high)
+        } else {setLevel(normal)}
+      }
+}, [afterMeal, bloodSugar])
 
   return (
     <CardGrid>
+      {/* // Play with these values to see how they render appropriately! Delete this entire div once information is successfully being retrieved from database */}
+      <div>
+        <h3>Temporary area to test "Levels" state, delete once we have this data coming from database</h3>
+        <p>Click to see the elements change!</p>
+        <button onClick={()=> setBloodSugar(bloodSugar + 10)}>Raise measurement number</button>
+        <button onClick={()=> setBloodSugar(bloodSugar - 10)}>Lower measurement number</button>
+        <button onClick={()=> setAfterMeal(!afterMeal)}>Switch before/after meal</button>
+      </div>
       <BloodSugarCard bloodSugar={bloodSugar} afterMeal={afterMeal}/>
       {level.warning !== "normal" && <WarningCard level={level.warning} title={level.research.title} subtitle={level.research.subtitle}  warning={level.research.warning} todos={level.research.todos} />}
       {level.warning !== "normal" && <SymptomsCard level={level.warning} title={level.symptoms.title} subtitle={level.symptoms.subtitle} summary={level.symptoms.summary} symptoms={level.symptoms.symptoms}/>}
