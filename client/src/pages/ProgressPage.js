97f6765cd12db8bfc8790f235b530acb7c5db9cc
Nathan Pecchia <septumprince@macbook-pro-2.home>
2020-09-11 22:50:20 -0400

    Seperated into states

--- client/src/pages/ProgressPage.js
+++ client/src/pages/ProgressPage.js
@@ -11,49 +11,63 @@ import { symptomsLBS, symptomsHBS } from "../research";
 import SymptomsCard from "../components/WarningComponents/SymptomsCard"
 
 function ProgressPage(props) {
+  const low = {
+    warning: "low",
+    research: treatingLBS,
+    symptoms: symptomsLBS
+  }
+  const high = {
+    warning: "high",
+    research: treatingHBS,
+    symptoms: symptomsHBS
+  }
   // Input Page
-  const [bloodSugar, setBloodSugar] = useState(180)
-  const [afterMeal, setAfterMeal] = useState(true)
+  const [bloodSugar, setBloodSugar] = useState()
+  const [afterMeal, setAfterMeal] = useState()
 
   // Progress Page
-  const [warning, setWarning] = useState("low")
-  const [research, setResearch] = useState(treatingLBS)
-  const [symptoms, setSymptoms] = useState(symptomsHBS)
+  const [level, setLevel] = useState(high)
 
-
+  // This may not be the appropriate use of useEffect, but should maybe be triggered as a callback function once our page recieves the last measurement from the databse.
+useEffect(() => {
+  setBloodSugar(180)
+  setAfterMeal(true)
+  // 
+// switch (afterMeal, bloodSugar) {
+//         case !afterMeal && bloodSugar < 130:
+//           setWarning("normal")
+//           break;
+//         case afterMeal && bloodSugar > 130:
+//           setWarning("normal")
+//           break;
+//         case !afterMeal && bloodSugar < 80:
+//           setWarning("low")
+//           break;
+//         case afterMeal && bloodSugar < 130:
+//           setWarning("low")
+//           break;
+//         case !afterMeal && bloodSugar > 130:
+//           setWarning("high")
+//           break;
+//         case afterMeal && bloodSugar > 180:
+//           setWarning("high")
+//           break;
+//         default:
+//           break;
+//       }
+})
 
 
 
   // const getLastMeasurement = () => {
   //     //  Once last measurement is retrieved from database
-  //     switch (key) {
-  //       case !afterMeal && bloodSugar < 130:
-  //         setWarning("normal")
-  //         break;
-  //       case afterMeal && bloodSugar > 130:
-  //         setWarning("normal")
-  //         break;
-  //       case !afterMeal && bloodSugar < 80:
-  //         setWarning("low")
-  //         break;
-  //       case afterMeal && bloodSugar < 130:
-  //         setWarning("low")
-  //         break;
-  //       case !afterMeal && bloodSugar > 130:
-  //         setWarning("high")
-  //         break;
-  //       case afterMeal && bloodSugar > 180:
-  //         setWarning("high")
-  //         break;
-  //       default:
-  //         break;
-  //     }
+  //    
 
   return (
     <CardGrid>
       <BloodSugarCard bloodSugar={bloodSugar} afterMeal={afterMeal}/>
-      {warning !== "normal" && <WarningCard level={warning} title={research.title} subtitle={research.subtitle}  warning={research.warning} todos={research.todos} />}
-      {warning !== "normal" && <SymptomsCard level={warning} title={symptoms.title} subtitle={symptoms.subtitle} summary={symptoms.summary} symptoms={symptoms.symptoms}/>}
+      {level.warning !== "normal" && <WarningCard level={level.warning} title={level.research.title} subtitle={level.research.subtitle}  warning={level.research.warning} todos={level.research.todos} />}
+      {level.warning !== "normal" && <SymptomsCard level={level.warning} title={level.symptoms.title} subtitle={level.symptoms.subtitle} summary={level.symptoms.summary} symptoms={level.symptoms.symptoms}/>}
       <FoodTrackCard />
       {/* <ChartCard /> */}
       <CareScheduleAccordion />
