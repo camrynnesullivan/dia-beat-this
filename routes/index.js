88a11b0980a1c582c178e717e34474913b0fbf83
Natallia Stelmakh <stnata.coding@gmail.com>
2020-09-13 01:38:38 -0400

    changes

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,11 @@ const Router = require("express").Router();
 const apiRoutes = require("./apiRoutes");
 const authRoutes = require("../authentication/authRoutes");
 const passport = require("../authentication/passport");
+const glycemiaRoutes = require("./glycemiaRoutes");
 
+Router.use("/api/glycemia", glycemiaRoutes);
 Router.use("/api", passport.authenticate("jwt", { session: false }), apiRoutes);
+
 Router.use("/auth", authRoutes);
 
 module.exports = Router;
