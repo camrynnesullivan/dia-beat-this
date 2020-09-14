c0840c2d28a55d0ac392991e9ee429dc8457dfae
Nathan Pecchia <septumprince@MacBook-Pro-2.local>
2020-09-14 13:20:16 -0400

    Fixed CSS of Complications card

--- client/src/components/InputComponents/InputGoalCard.js
+++ client/src/components/InputComponents/InputGoalCard.js
@@ -1,7 +1,6 @@
 import React, {useState} from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import TextField from '@material-ui/core/TextField';
-import Button from '@material-ui/core/Button';
 import FormLabel from '@material-ui/core/FormLabel';
 import FormGroup from '@material-ui/core/FormGroup';
 import InputFormGrid from './InputFormGrid';
