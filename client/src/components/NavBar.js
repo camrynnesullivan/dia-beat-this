3f15120b4f5873f021bc3f2da0f4f0ef550f7b64
Hachem Said <labsar.ahmed@outlook.fr>
2020-09-21 19:17:25 -0400

    deleted chat from navbar

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -76,11 +76,6 @@ const NavBar = (props) => {
       menuTitle: "Journal",
       pageURL: "/Journal",
     },
-    {
-      id: "6",
-      menuTitle: "Chat",
-      pageURL: "/Chat",
-    },
   ];
 
   let headerTitle;
